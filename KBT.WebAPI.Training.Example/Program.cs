6803726dae4f0e1222e3274c2a023883ffd017f4
SurasithIT <59070181@it.kmitl.ac.th>
2022-04-02 14:44:36 +0700

    Add logging using Log4Net

--- KBT.WebAPI.Training.Example/Program.cs
+++ KBT.WebAPI.Training.Example/Program.cs
@@ -1,37 +1,57 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection;
+using System.Xml;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add logging
+log4net.ILog logger = log4net.LogManager.GetLogger("Startup");
+log4net.ThreadContext.Properties["threadid"] = Thread.CurrentThread.ManagedThreadId;
 
-// Add CORS
-builder.Services.AddCors(options =>
+XmlDocument log4netConfig = new XmlDocument();
+log4netConfig.Load(File.OpenRead("log4net.config"));
+var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(),
+           typeof(log4net.Repository.Hierarchy.Hierarchy));
+log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+
+try
 {
-    options.AddPolicy("AllowAnyOrigin",
-        builder => builder.SetIsOriginAllowed(x => _ = true)
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-});
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Add CORS
-app.UseCors("AllowAnyOrigin");
+    // Add CORS
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAnyOrigin",
+            builder => builder.SetIsOriginAllowed(x => _ = true)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+    });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var app = builder.Build();
+
+    // Add CORS
+    app.UseCors("AllowAnyOrigin");
+
+    logger.Debug("Environment is : " + app.Environment.EnvironmentName);
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
+catch (Exception ex)
+{
+    logger.Error(ex.Message, ex);
+}
